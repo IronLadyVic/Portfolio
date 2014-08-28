2e8e82fec1f22b9759ec893660da4ba87956a71f
unknown <victoria.clark@44385-WIN-501.auckland.natcoll.ac.nz>
2014-08-29 11:59:55 +1200

    second commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ $(function(){
 	$(".flex-item").on("click", function(){
 
 		$(this).children("span").addClass("show");
+		$(".flex-item").not(this).children("span").removeClass("show");
 
 	});
 
@@ -14,63 +15,16 @@ $(function(){
 	});
 
 });
-//Controlling what is clicked on and what is not
-// $(".flex-item span").hide();
-//close other opened items
-// $(".flex-item span").on("click",function(){
-// 	$(".flex-item span").not(this).next().slideUp(function(){
-// 		$(this).prev().find("span").removeClass("show").addClass("show");
-// 		$(this).prev().data("open",0);	
-
-// 	});
-//Controlling the item being clicked on.
-// if($(this).data("open")==1){
-// 	$(this).next().slideUp(function(){
-
-// 		$(this).prev().find("span").removeClass("show")
-// 		.addClass("show");
-// 		$(this).prev().data("open",0);
-// 	});
-
-// }else{
-// 	$(this).next().slideDown(function(){
-
-// 		$(this).prev().find("span").removeClass("show")
-// 		.addClass("span");
-// 		$(this).prev().data("open",1);
-// 	});
-// }
-
-// });
-
-
-
-
 
 
 //Drop down menu
 
 $(function(){
-	var bOpen = false;
+	//var bOpen = false;
 
 	$(".nav-handle a").on("click", function(){
-		//close nav bar
-		if(bOpen == true){
-		$("#main-nav").animate({height:"0em", width: "0em"},1000,function(){
-			$("#main-nav li").removeClass("show")
-										.addClass("show");
-			bOpen = false;
-		});
-		//open nav bar
-	}else{
-		$("#main-nav").animate("slow", function(){
-					//do this after animation has completed
-					$("#main-nav li").removeClass("show")
-										.addClass("show");
 
-					bOpen = true;					
-				});
-			}
+		$("#main-nav li").toggleClass("show");
 
 	});
 	
